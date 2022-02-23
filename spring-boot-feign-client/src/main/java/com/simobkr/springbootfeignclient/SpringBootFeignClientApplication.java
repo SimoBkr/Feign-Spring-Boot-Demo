b8909be572779fdee4b81bcf6b1b79938fefa754
Mohamed Bakadir <SimoBkr@users.noreply.github.com>
2022-02-23 11:03:59 +0100

    using feign to consume an external api

--- spring-boot-feign-client/src/main/java/com/simobkr/springbootfeignclient/SpringBootFeignClientApplication.java
+++ spring-boot-feign-client/src/main/java/com/simobkr/springbootfeignclient/SpringBootFeignClientApplication.java
@@ -2,8 +2,10 @@ package com.simobkr.springbootfeignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringBootFeignClientApplication {
 
 	public static void main(String[] args) {
---
+++ spring-boot-feign-client/src/main/java/com/simobkr/springbootfeignclient/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.simobkr.springbootfeignclient.client;
+
+import com.simobkr.springbootfeignclient.dto.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com",name = "USER-CLIENT")
+public interface UserClient {
+
+    @GetMapping("/users")
+    List<UserResponse> getAllUsers();
+
+}
